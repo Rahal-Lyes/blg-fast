04fac7d23ae3b53bebff681de02f543d9fa6a9b2
Lyes Rahal <lyes.rahal@univ-bouira.dz>
2026-06-17 21:24:32 +0100

    update

--- app/db/database.py
+++ app/db/database.py
@@ -5,11 +5,19 @@ from app.core.config import settings
 engine = create_engine(
     settings.DATABASE_URL,
     pool_pre_ping=True,
-    pool_size=10,
-    max_overflow=20,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=10,
+    connect_args={
+        "sslmode": "require"
+    }
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 
 class Base(DeclarativeBase):
